ccdbe099a57baed795e54cbed07e575c99c29354
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-11-21 16:05:22 +0530

    Add bottombar navigation links

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,5 +1,34 @@
+import { bottombarLinks } from "@/constants";
+import { INavLink } from "@/types";
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Bottombar() {
-	return <div>Bottombar</div>;
+	const { pathname } = useLocation();
+	return (
+		<section className="bottom-bar">
+			{bottombarLinks.map((link) => (
+				<Link
+					to={link.route}
+					className={`flex-center flex-col gap-1 p-2 transition ${
+						pathname === link.route
+							? "bg-primary-500 rounded-[10px]"
+							: ""
+					}`}
+					key={link.label}
+				>
+					<img
+						src={link.imgURL}
+						alt={link.label}
+						width={16}
+						height={16}
+						className={`${
+							pathname === link.route ? "invert-white" : ""
+						}`}
+					/>
+					<p className="tiny-medium text-light-2">{link.label}</p>
+				</Link>
+			))}
+		</section>
+	);
 }
