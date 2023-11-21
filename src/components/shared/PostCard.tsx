83a4f8600872fc81823bf19fa518cd4b7cbc7a54
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-11-21 20:36:47 +0530

    Fix missing key and fix getFilePreview Func and Render Image in Post

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,8 +1,12 @@
+import { useUserContext } from "@/context/AuthContext";
 import { formatDate } from "@/lib/utils";
 import { IPostCardProps } from "@/types";
 import { Link } from "react-router-dom";
 
 export default function PostCard({ post }: IPostCardProps) {
+	const { user } = useUserContext();
+
+	if (!post.creator) return;
 	return (
 		<div className="post-card">
 			<div className="flex-between">
@@ -29,7 +33,39 @@ export default function PostCard({ post }: IPostCardProps) {
 						</div>
 					</div>
 				</div>
+
+				<Link
+					to={`/update-post/${post.$id}`}
+					className={`${user.id !== post.creator.$id && "hidden"}`}
+				>
+					<img
+						src="/assets/icons/edit.svg"
+						alt="edit"
+						width={20}
+						height={20}
+					/>
+				</Link>
 			</div>
+
+			<Link to={`/post/${post.$id}`}>
+				<div className="small-medium lg:base-medium py-5">
+					<p>{post.caption}</p>
+					<ul className="flex gap-1 mt-2">
+						{post.tags.map((tag: string) => (
+							<li key={tag} className="text-light-3">
+								#{tag}
+							</li>
+						))}
+					</ul>
+				</div>
+				<img
+					src={
+						post.imageUrl || "/assets/icons/profile-placeholder.svg"
+					}
+					alt="post image"
+					className="post-card_img"
+				/>
+			</Link>
 		</div>
 	);
 }
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -154,9 +154,9 @@ export async function uploadFile(file: File) {
     }
 }
 
-export async function getFilePreview(fileId: string) {
+export function getFilePreview(fileId: string) {
     try {
-        const fileUrl = await storage.getFilePreview(
+        const fileUrl = storage.getFilePreview(
             appwriteConfig.storageId,
             fileId,
             2000, 2000, "top", 100,
